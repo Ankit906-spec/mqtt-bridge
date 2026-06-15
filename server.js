b85fee43f619fed62685eb08e1d0e0a0f6877110
Ankit906-spec <ankityadav94698@gmail.com>
2026-06-15 16:59:43 +0530

    Initialize Express server with MQTT integration

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const mqtt = require("mqtt");
+
+const app = express();
+app.use(express.json());
+
+const client = mqtt.connect({
+  host: process.env.MQTT_SERVER,
+  port: process.env.MQTT_PORT,
+  username: process.env.MQTT_USER,
+  password: process.env.MQTT_PASSWORD,
+  protocol: "mqtts"
+});
+
+client.on("connect", () => {
+  console.log("Connected to HiveMQ");
+});
+
+app.post("/order", (req, res) => {
+  client.publish(process.env.MQTT_TOPIC, "F");
+  res.send("OK");
+});
+
+app.post("/stop", (req, res) => {
+  client.publish(process.env.MQTT_TOPIC, "S");
+  res.send("STOP");
+});
+
+app.listen(process.env.PORT || 10000, () => {
+  console.log("Server Running");
+});
