5fb8398662d6f9fc2bf578de4ecfdc243e320fad
Ankit906-spec <ankityadav94698@gmail.com>
2026-06-15 17:58:53 +0530

    Set up MQTT connection and order API endpoint

--- server.js
+++ server.js
@@ -1,3 +1,21 @@
+const express = require("express");
+const mqtt = require("mqtt");
+
+const app = express();
+app.use(express.json());
+
+const client = mqtt.connect({
+  host: process.env.MQTT_SERVER,
+  port: process.env.MQTT_PORT,
+  username: process.env.MQTT_USER,
+  password: process.env.MQTT_PASSWORD,
+  protocol: "mqtts"
+});
+
+client.on("connect", () => {
+  console.log("Connected to HiveMQ");
+});
+
 app.post("/order", (req, res) => {
   console.log("Order API was called");
   console.log(req.body);
@@ -17,4 +35,8 @@ app.post("/stop", (req, res) => {
   console.log("Published: S");
 
   res.send("STOP");
+});
+
+app.listen(process.env.PORT || 10000, () => {
+  console.log("Server Running");
 });
