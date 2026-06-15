258be287c1b675396a213513c6ef13caf2326c32
Ankit906-spec <ankityadav94698@gmail.com>
2026-06-15 17:53:30 +0530

    Refactor order and stop API endpoints in server.js
    Refactored server.js to streamline order and stop API endpoints and removed MQTT connection setup.

--- server.js
+++ server.js
@@ -1,31 +1,20 @@
-const express = require("express");
-const mqtt = require("mqtt");
-
-const app = express();
-app.use(express.json());
-
-const client = mqtt.connect({
-  host: process.env.MQTT_SERVER,
-  port: process.env.MQTT_PORT,
-  username: process.env.MQTT_USER,
-  password: process.env.MQTT_PASSWORD,
-  protocol: "mqtts"
-});
-
-client.on("connect", () => {
-  console.log("Connected to HiveMQ");
-});
-
 app.post("/order", (req, res) => {
+  console.log("Order API was called");
+  console.log(req.body);
+
   client.publish(process.env.MQTT_TOPIC, "F");
+
+  console.log("Published: F");
+
   res.send("OK");
 });
 
 app.post("/stop", (req, res) => {
-  client.publish(process.env.MQTT_TOPIC, "S");
-  res.send("STOP");
-});
+  console.log("Stop API was called");
 
-app.listen(process.env.PORT || 10000, () => {
-  console.log("Server Running");
+  client.publish(process.env.MQTT_TOPIC, "S");
+
+  console.log("Published: S");
+
+  res.send("STOP");
 });
